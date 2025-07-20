830e0223703454cf3fb60a64cb72a53870fccd9b
G3kan <104479330+G3kan@users.noreply.github.com>
2025-07-20 19:39:50 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -76,7 +76,23 @@ def create_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_accounts(account_id):
+    """
+    Reads an Account
+    This endpoint will read an Account based the account_id that is requested
+    """
+    app.logger.info("Request to read an Account with id: %s", account_id)
 
+    # Account.find() metodunu kullanarak hesabı ID'ye göre buluyoruz
+    account = Account.find(account_id)
+
+    # Eğer hesap bulunamazsa 404 Not Found hatası döndürüyoruz
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+
+    # Hesabı serialize edip 200 OK durum kodu ile geri döndürüyoruz
+    return account.serialize(), status.HTTP_200_OK
 
 
 
