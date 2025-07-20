5357c92737a7ab1e46bb57261e5388af020a6fbf
G3kan <104479330+G3kan@users.noreply.github.com>
2025-07-20 20:53:57 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -77,7 +77,7 @@ def list_accounts():
 ######################################################################
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-    def get_accounts(account_id):
+def get_accounts(account_id):
         """
         Reads an Account
         This endpoint will read an Account based the account_id that is requested
@@ -95,7 +95,7 @@ def list_accounts():
 ######################################################################
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
-    def update_accounts(account_id):
+def update_accounts(account_id):
         """
         Update an Account
         This endpoint will update an Account based on the posted data
@@ -115,7 +115,7 @@ def list_accounts():
 ######################################################################
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-    def delete_accounts(account_id):
+def delete_accounts(account_id):
         """
         Delete an Account
         This endpoint will delete an Account based on the account_id that is requested
